bd5c3a7a4f793099f518e36ca718bc1865948037
konstin <konstin@mailbox.org>
2023-06-01 13:56:07 +0200

    Implement basic module formatting
    Getting a feel for the type system.
    
    This doesn't actually implement the desired formatting yet, i've updated the snapshots nevertheless.

--- crates/ruff_python_formatter/src/module/mod_module.rs
+++ crates/ruff_python_formatter/src/module/mod_module.rs
@@ -1,5 +1,9 @@
-use crate::{verbatim_text, FormatNodeRule, PyFormatter};
-use ruff_formatter::{write, Buffer, FormatResult};
+use crate::AsFormat;
+use crate::{FormatNodeRule, PyFormatter};
+use ruff_formatter::formatter::Formatter;
+use ruff_formatter::prelude::{empty_line, hard_line_break};
+use ruff_formatter::{write, Buffer, Format, FormatContext, FormatResult};
+use ruff_python_ast::prelude::Stmt;
 use rustpython_parser::ast::ModModule;
 
 #[derive(Default)]
@@ -7,6 +11,27 @@ pub struct FormatModModule;
 
 impl FormatNodeRule<ModModule> for FormatModModule {
     fn fmt_fields(&self, item: &ModModule, f: &mut PyFormatter) -> FormatResult<()> {
-        write!(f, [verbatim_text(item.range)])
+        for stmt in &item.body {
+            match stmt {
+                Stmt::FunctionDef(function_def) => write!(f, [TwoNewlinesAfter(function_def)])?,
+                Stmt::AsyncFunctionDef(async_function_def) => {
+                    write!(f, [TwoNewlinesAfter(async_function_def)])?
+                }
+                Stmt::ClassDef(class_def) => write!(f, [TwoNewlinesAfter(class_def)])?,
+                Stmt::Assign(assign) => write!(f, [assign.format(), hard_line_break()])?,
+                Stmt::AugAssign(assign) => write!(f, [assign.format(), hard_line_break()])?,
+                Stmt::AnnAssign(assign) => write!(f, [assign.format(), hard_line_break()])?,
+                _ => write!(f, [stmt.format(), empty_line()])?,
+            }
+        }
+        Ok(())
+    }
+}
+
+pub(crate) struct TwoNewlinesAfter<T>(T);
+
+impl<C: FormatContext, T: AsFormat<C>> Format<C> for TwoNewlinesAfter<T> {
+    fn fmt(&self, f: &mut Formatter<C>) -> FormatResult<()> {
+        write!(f, [self.0.format(), empty_line(), empty_line()])
     }
 }
